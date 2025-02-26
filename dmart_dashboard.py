f280ab4c575d0b9ecf9db17d09c258ebc4027c81
ambition03 <akanksha.yadav@innoviti.com>
2025-02-26 15:35:50 +0530

    Update dmart_dashboard.py

--- dmart_dashboard.py
+++ dmart_dashboard.py
@@ -7,6 +7,14 @@ import re
 import spacy
 from textblob import TextBlob
 from sklearn.preprocessing import LabelEncoder
+import spacy
+import os
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # Load NLP model for query understanding
 nlp = spacy.load("en_core_web_sm")
